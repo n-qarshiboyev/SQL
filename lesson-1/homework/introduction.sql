05790eb1e31af448e6fb93c400c6893581ebd5ed
Nurmuhammad <nqarshiboyev@icloud.com>
2025-04-25 13:29:01 +0500

    Create introduction.sql

---
+++ lesson-1/homework/introduction.sql
@@ -0,0 +1,31 @@
+Introduction to BI
+
+What is BI (Business Intelligence)
+
+   - It is a strategy that Data Analysts use to prevent loss or breakeven and improve profit in a given business. 
+
+It contains 4 steps: 
+#1 Data Collection - It’s the process of evaluating information or data from multiple sources. 
+#2 Analysis - is the process of applying statical or logical techniques to recap the date collected. 
+#3 Visualization - It’s representation of the recapped data found by creating charts, graphs or maps.
+#4 Setting the stage for final decision - Providing the found data to the business worker/owner to make the final move
+
+
+Data can be divided to two groups 
+
+Structured - Can be displayed in rows, columns and numbers and strings (texts). This data is found in 20% of all data so It requires less storage than unstructured data while also being easy to manage. 
+
+Unstructured - The data which can’t displayed in rows or columns (etc) since these are mostly images, audios, videos, files or emails. 
+Therefore, it usually takes lots of storage and usually found in 80% of all data. While being uneasy to handle. 
+
+These two above can also divided by being Quantitive or Qualitative. 
+
+Quantitative data is the data that can be counted whether it’s money, time or measurements. 
+
+Qualitative data is the data that cannot be counted as it’s rather about verbal or written feedback, story or descriptions of colors and other qualities which can’t be measured by numbers. 
+
+
+
+
+
+
