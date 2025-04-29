d73bb55e2bfff2352409f0e234c6cf770084de5b
Nurmuhammad <nqarshiboyev@icloud.com>
2025-04-29 07:41:39 -0400

    homework.sql

--- lesson-1/homework/introduction.sql
+++ lesson-1/homework/introduction.sql
@@ -1,31 +1,52 @@
-Introduction to BI
+--Easy
+Data - is the raw collected facts about any given business / work.
+Database - It is thee collection of data where speecific raw facts are stored in the system. 
+Relational database - is a type of database that have structures to allow users to identify which data is related to any other data.
+Table - is a place where information are stored, For example, a business can store their customers contacts, emails, addresses on a table to access it easily when needed.
 
-What is BI (Business Intelligence)
+The most important features of SQL Server are:
+   It can be accessed from anywhere with login & password
+   Several people can edit. add or remove data at the same time
+   It does not require powerful PC or laptop, it can be used on an average device
+   The added data is always encrypted and secured
+   The overall performance is smooth and the ui is user-friendly
 
-   - It is a strategy that Data Analysts use to prevent loss or breakeven and improve profit in a given business. 
-
-It contains 4 steps: 
-#1 Data Collection - It’s the process of evaluating information or data from multiple sources. 
-#2 Analysis - is the process of applying statical or logical techniques to recap the date collected. 
-#3 Visualization - It’s representation of the recapped data found by creating charts, graphs or maps.
-#4 Setting the stage for final decision - Providing the found data to the business worker/owner to make the final move
-
-
-Data can be divided to two groups 
-
-Structured - Can be displayed in rows, columns and numbers and strings (texts). This data is found in 20% of all data so It requires less storage than unstructured data while also being easy to manage. 
-
-Unstructured - The data which can’t displayed in rows or columns (etc) since these are mostly images, audios, videos, files or emails. 
-Therefore, it usually takes lots of storage and usually found in 80% of all data. While being uneasy to handle. 
-
-These two above can also divided by being Quantitive or Qualitative. 
-
-Quantitative data is the data that can be counted whether it’s money, time or measurements. 
-
-Qualitative data is the data that cannot be counted as it’s rather about verbal or written feedback, story or descriptions of colors and other qualities which can’t be measured by numbers. 
+There are a lot of authentication modes from Microsoft but the most used ones are from Windows (Windows Authentication) and SQl Server Authentication.
 
 
 
+--Medium
+create database SchoolDB
+create table Students(ID int, Name varchar (50), Age int)
 
 
 
+SQL Server is a server where users can store their database, SSMS (SQL Server Management Studio) is an app where that server can be connected. SQL (Structured Queery Language) is a computer language used to 
+manage Data.
+
+--Hard
+DQL stands for "Data Query Language". One of most used commands of this is "Select" Which is similar to a command called "print". It is used to show selected text or number when executed.
+
+   SELECT column1, column2, ...
+   
+DML stands for "Data Manipulation Language" Most used DML commands are: insert (to add a new row), update (used to modify the records), delete - sometimes select is also used. 
+
+   INSERT INTO table_name (column1, column2, column3, ...) VALUES (value1, value2, value3, ...);
+   UPDATE table_name SET column1 = value1, column2 = value2, ...
+   DELETE FROM table_name   
+   
+DDL stands for "Data Definition Language" commands are: create (to make new objects like database, tables and etc), alter (to modify existing database object), drop (deletes selected table, views)
+
+      CREATE TABLE table_name
+      ALTER TABLE table_name ADD column_name datatype;
+   
+DCL stands for "Data Control Language" it uses these commands: grant (to give access to users to the database) and revoke (to do the opposite of grant)
+
+   GRANT privilege_name ON object_name TO user_name; 
+   REVOKE privilege_name ON object_name FROM user_name;
+
+TCL stands for "Transaction Control Language" and it uses these: begin transaction (Sometimes just begin, starts new transaction), commit (Saves all changes), rollback (undoes all changes that were made)
+   
+   begin transaction
+   commit
+   rollback
