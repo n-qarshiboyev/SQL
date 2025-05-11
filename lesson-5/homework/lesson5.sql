584b729a7ec65ee54a26df9f7d29664f32d31416
Nurmuhammad <nqarshiboyev@icloud.com>
2025-05-12 01:08:35 +0500

    Update lesson5.sql

--- lesson-5/homework/lesson5.sql
+++ lesson-5/homework/lesson5.sql
@@ -34,5 +34,34 @@
 10) Create a conditional column using IIF that shows 'Expensive' if the Price is greater than 1000, and 'Affordable' if less, from Products table.
   select IIF(Price > 1000, 'Expensive', 'Affordable') from Products;
 
-11) 
+11) Write a query to find employees in the Employees table who have either Age < 25 or Salary > 60000.
+  select * from Employees where Age < 25 or Salary > 60000;
 
+12) Update the salary of an employee based on their department, increase by 10% if they work in 'HR' or EmployeeID = 5
+  update Employees set Salary = Salary * 1.10 where DepartmentName = 'HR' or EmployeeID = 5;
+
+--Hard-Level Tasks
+
+13) Write a query that uses CASE to assign 'Top Tier' if SaleAmount > 500, 'Mid Tier' if SaleAmount BETWEEN 200 AND 500, and 'Low Tier' otherwise. (From Sales table)
+
+select *, 
+    case 
+	    when SaleAmount > 500 then 'Top Tier'
+      when SaleAmount between 200 and 500 then 'Mid Tier'
+      else 'Low Tier' end as TierList from Sales; 
+
+14) Use EXCEPT to find customers ID who have placed orders but do not have a corresponding record in the Sales table.
+  select CustomerID from Orders
+    except
+    select CustomerID from Sales;
+
+15) Write a query that uses a CASE statement to determine the discount percentage based on the quantity purchased. Use orders table. Result set should show customerid, quantity and discount percentage. The discount should be applied as follows: 1 item: 3% Between 1 and 3 items : 5% Otherwise: 7%
+  select 
+    CustomerID,
+    Quantity,
+    case 
+        when Quantity = 1 then '3%'
+        when Quantity > 1 and Quantity <= 3 then '5%'
+        else '7%'
+    end as DiscountPercentage
+from Orders;
